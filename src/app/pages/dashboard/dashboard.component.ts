7d73459bd788ffe6413b1fb2b319b76ccac6efd7
Myrna <myrnapt96@gmail.com>
2023-11-14 23:31:50 +0100

    end

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pages-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(private el: ElementRef) {}
 
-  
+  ngOnInit() {
+    // Inicia la animación cuando el componente se inicializa
+    this.startAnimation();
+  }
+
+  private startAnimation() {
+    // Obtén la referencia al elemento principal con la clase .main
+    const mainElement = this.el.nativeElement.querySelector('.main');
+
+    // Agrega la clase de animación para iniciar la animación zoom
+    mainElement.classList.add('animate');
+
+    // Espera un breve período antes de agregar la clase a star-wars para sincronizar las animaciones
+    setTimeout(() => {
+      const starWarsSection = this.el.nativeElement.querySelector('.star-wars');
+      starWarsSection.classList.add('animate');
+    }, 500);
+  }
 }
